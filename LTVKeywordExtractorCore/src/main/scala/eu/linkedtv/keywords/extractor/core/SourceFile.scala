677b46c015d6246f1cb0d2faf748b8cff729790f
propi <prozeman@gmail.com>
2014-09-25 16:44:44 +0200

    Euro detection added

--- LTVKeywordExtractorCore/src/main/scala/eu/linkedtv/keywords/extractor/core/SourceFile.scala
+++ LTVKeywordExtractorCore/src/main/scala/eu/linkedtv/keywords/extractor/core/SourceFile.scala
@@ -3,58 +3,63 @@ package eu.linkedtv.keywords.extractor.core
 import eu.linkedtv.keywords.extractor.core.impl.Occurence
 import eu.linkedtv.keywords.extractor.core.impl.Successor
 
-case class KeywordOccurence(val start : Long, val end : Long)
+case class KeywordOccurence(val start: Long, val end: Long)
 
-class SourceFile(val fileName : String, val lang : KELanguage, val source : Array[Byte])
+class SourceFile(val fileName: String, val lang: KELanguage, val source: Array[Byte])
 
-class ResultFile(val tokenizer : Tokenizer) {
-  
+class ResultFile(val tokenizer: Tokenizer) {
+
   lazy val wordMap = tokenizer.tokens groupBy (_.word.toLowerCase)
-  
-  def keywordOccurences(keyword : String) = { 
+
+  def keywordOccurences(keyword: String) = {
     val words = keyword.split(' ').toList
     wordMap.get(words.head.toLowerCase) match {
-      case Some(tokens) => 
+      case Some(tokens) =>
         (
           tokens map {
             case token @ Occurence(occurStart) => {
-                def walkKeyword(currToken : Token, input : List[String]) : Option[KeywordOccurence] = (currToken, input) match {
-                  case (Successor(succ), head :: (tail @ (second :: _))) if currToken.word.toLowerCase == head.toLowerCase => walkKeyword(succ.token, tail)
-                  case (Occurence(occurEnd), head :: Nil) if currToken.word.toLowerCase == head.toLowerCase => Some(KeywordOccurence(occurStart.start, occurEnd.end))
-                  case _ => None
-                }
+              def walkKeyword(currToken: Token, input: List[String]): Option[KeywordOccurence] = (currToken, input) match {
+                case (Successor(succ), head :: (tail @ (second :: _))) if currToken.word.toLowerCase == head.toLowerCase => walkKeyword(succ.token, tail)
+                case (Occurence(occurEnd), head :: Nil) if currToken.word.toLowerCase == head.toLowerCase => Some(KeywordOccurence(occurStart.start, occurEnd.end))
+                case _ => None
+            }
                 walkKeyword(token, words)
               }
             case _ => None
-          } collect { case Some(x) => x }
-        ).toSet
+          } collect { case Some(x) => x }).toSet
       case _ => Set.empty
     }
   }
-  
+
 }
 
 object SourceFile {
-  
-  def learn(ke : KeywordExtractor, tsb : TokenizerStringBuilder)(src : SourceFile, rsf : ResultFile) = ke.putFile(src.fileName, src.lang, tsb.toString(rsf.tokenizer))
-    
-  def result(ke : KeywordExtractor)(id : Int, rsf : ResultFile) = 
+
+  def learn(ke: KeywordExtractor, tsb: TokenizerStringBuilder)(src: SourceFile, rsf: ResultFile) = ke.putFile(src.fileName, src.lang, tsb.toString(rsf.tokenizer))
+
+  def result(ke: KeywordExtractor)(id: Int, rsf: ResultFile) =
     <keyword_extraction>
       <keywords>
         {
           ke.fetchKeywords(id) map (kw =>
-            <keyword word={kw.word}>
+            <keyword word={ kw.word }>
               <occurrences>
                 {
                   rsf.keywordOccurences(kw.word) map (oc =>
-                    <occurrence start={oc.start.toString} end={oc.end.toString} />
-                  )
+                    <occurrence start={ oc.start.toString } end={ oc.end.toString }/>)
                 }
               </occurrences>
-            </keyword>
-          )
+            </keyword>)
         }
       </keywords>
+      <euro>
+        <occurrences>
+          {
+            rsf.keywordOccurences("euro") map (oc =>
+              <occurrence start={ oc.start.toString } end={ oc.end.toString }/>)
+          }
+        </occurrences>
+      </euro>
     </keyword_extraction>
-  
+
 }
